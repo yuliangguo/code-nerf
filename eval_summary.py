f8e6e3d389a4b1a1b9d508a592a03dee80aa1ca1
Yuliang Guo <33yuliangguo@gmail.com>
2022-08-10 21:52:56 -0700

    add simple summary script to parse saved eval result

---
+++ eval_summary.py
@@ -0,0 +1,16 @@
+import torch
+import numpy as np
+
+if __name__ == '__main__':
+    result_file = './exps/srncar/test_rpert0.1_tpert0.1_nops200_nview2/codes+poses.pth'
+
+    saved_result = torch.load(result_file, map_location=torch.device('cpu'))
+
+    R_err_all = []
+    T_err_all = []
+    for R_err in saved_result['R_eval'].values():
+        R_err_all.append(R_err[0].squeeze().numpy())
+    for T_err in saved_result['T_eval'].values():
+        T_err_all.append(T_err[0].squeeze().numpy())
+
+    print(f'Avg R error: {np.mean(np.array(R_err_all))}, Avg T error: {np.mean(np.array(T_err_all))}')
