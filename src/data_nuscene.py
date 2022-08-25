7812e9024d3074a0bbfbd94ade993d3b9edf175b
Yuliang Guo <33yuliangguo@gmail.com>
2022-08-25 12:04:34 -0700

    add pred instance visualization for nuscence data

--- src/data_nuscene.py
+++ src/data_nuscene.py
@@ -6,9 +6,136 @@ import torch
 # from torchvision import transforms
 import os
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from PIL import Image
 from nuscenes.nuscenes import NuScenes
-from nuscenes.utils.geometry_utils import BoxVisibility
+from nuscenes.utils.geometry_utils import BoxVisibility, view_points
+from cityscapesscripts.helpers.labels import labels, name2label
+
+
+# Reference: https://github.com/facebookresearch/detectron2/blob/master/detectron2/utils/colormap.py#L14
+_COLORS = np.array(
+    [
+        0.000, 0.447, 0.741,
+        0.850, 0.325, 0.098,
+        0.929, 0.694, 0.125,
+        0.494, 0.184, 0.556,
+        0.466, 0.674, 0.188,
+        0.301, 0.745, 0.933,
+        0.635, 0.078, 0.184,
+        0.300, 0.300, 0.300,
+        0.600, 0.600, 0.600,
+        1.000, 0.000, 0.000,
+        1.000, 0.500, 0.000,
+        0.749, 0.749, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 1.000,
+        0.667, 0.000, 1.000,
+        0.333, 0.333, 0.000,
+        0.333, 0.667, 0.000,
+        0.333, 1.000, 0.000,
+        0.667, 0.333, 0.000,
+        0.667, 0.667, 0.000,
+        0.667, 1.000, 0.000,
+        1.000, 0.333, 0.000,
+        1.000, 0.667, 0.000,
+        1.000, 1.000, 0.000,
+        0.000, 0.333, 0.500,
+        0.000, 0.667, 0.500,
+        0.000, 1.000, 0.500,
+        0.333, 0.000, 0.500,
+        0.333, 0.333, 0.500,
+        0.333, 0.667, 0.500,
+        0.333, 1.000, 0.500,
+        0.667, 0.000, 0.500,
+        0.667, 0.333, 0.500,
+        0.667, 0.667, 0.500,
+        0.667, 1.000, 0.500,
+        1.000, 0.000, 0.500,
+        1.000, 0.333, 0.500,
+        1.000, 0.667, 0.500,
+        1.000, 1.000, 0.500,
+        0.000, 0.333, 1.000,
+        0.000, 0.667, 1.000,
+        0.000, 1.000, 1.000,
+        0.333, 0.000, 1.000,
+        0.333, 0.333, 1.000,
+        0.333, 0.667, 1.000,
+        0.333, 1.000, 1.000,
+        0.667, 0.000, 1.000,
+        0.667, 0.333, 1.000,
+        0.667, 0.667, 1.000,
+        0.667, 1.000, 1.000,
+        1.000, 0.000, 1.000,
+        1.000, 0.333, 1.000,
+        1.000, 0.667, 1.000,
+        0.333, 0.000, 0.000,
+        0.500, 0.000, 0.000,
+        0.667, 0.000, 0.000,
+        0.833, 0.000, 0.000,
+        1.000, 0.000, 0.000,
+        0.000, 0.167, 0.000,
+        0.000, 0.333, 0.000,
+        0.000, 0.500, 0.000,
+        0.000, 0.667, 0.000,
+        0.000, 0.833, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 0.167,
+        0.000, 0.000, 0.333,
+        0.000, 0.000, 0.500,
+        0.000, 0.000, 0.667,
+        0.000, 0.000, 0.833,
+        0.000, 0.000, 1.000,
+        0.000, 0.000, 0.000,
+        0.143, 0.143, 0.143,
+        0.857, 0.857, 0.857,
+        1.000, 1.000, 1.000
+    ]
+).astype(np.float32).reshape(-1, 3)
+
+# label encoding based on cityscape format
+def pan2img(pan):
+    # unique image encoding of a large iD int into RGB channels
+    img = np.zeros((pan.shape[0], pan.shape[1], 3), dtype=np.uint8)
+    img[:, :, 0] = pan % 256
+    img[:, :, 1] = (pan // 256) % 256
+    img[:, :, 2] = (pan // 256 // 256) % 256
+    return img
+
+
+# label encoding based on cityscape format
+def img2pan(img):
+    assert isinstance(img, np.ndarray) and len(img.shape) == 3 and img.shape[2] == 3
+    pan = img[:, :, 0] + 256 * img[:, :, 1] + 256 * 256 * img[:, :, 2]
+    return pan
+
+
+def pan2ins_vis(pan, cat_id, divisor):
+    img = np.zeros((pan.shape[0], pan.shape[1], 3), dtype=np.float32)
+    pixels = (pan // divisor) == cat_id
+    if np.sum(pixels) == 0:
+        return img
+    ins_ids = np.unique(pan[pixels])
+    for ins_id in ins_ids:
+        yy, xx = np.where(pan == ins_id)
+        img[yy, xx, :] = _COLORS[ins_id % divisor]
+    return img
+
+
+def tgt_instance(pan, cat_id, box):
+    # return the instance label and pixel counts in the box
+    min_x, min_y, max_x, max_y = box
+    box_pan = pan[int(min_y):int(max_y), int(min_x):int(max_x)]
+    ins_ids, cnts = np.unique(box_pan, return_counts=True)
+    tgt_ins_id = np.where((ins_ids // divisor) == cat_id)[0]
+    if len(tgt_ins_id) == 0:
+        return None, 0
+
+    ins_ids = ins_ids[tgt_ins_id]
+    cnts = cnts[tgt_ins_id]
+    max_id = np.argmax(cnts)
+    return ins_ids[max_id], cnts[max_id]
+
 
 def load_poses(pose_dir, idxs=[]):
     txtfiles = np.sort([os.path.join(pose_dir, f.name) for f in os.scandir(pose_dir)])
@@ -94,6 +221,9 @@ class NuScenesData():
 
 if __name__ == '__main__':
     tgt_category = 'vehicle.car'
+    cityscape_cat = 'car'
+    divisor = 1000
+    panoptic_folder = '/mnt/LinuxDataFast/Datasets/NuScenes/v1.0-mini/panoptic_pred'
     nusc = NuScenes(version='v1.0-mini', dataroot='/mnt/LinuxDataFast/Datasets/NuScenes/v1.0-mini', verbose=True)
     nusc.list_scenes()
     instance_all = nusc.instance
@@ -111,7 +241,7 @@ if __name__ == '__main__':
 
         # For each annotation get all the sensors
         for anntoken in ann_tokens:
-            print(f'    qanntoken: {anntoken}')
+            print(f'    anntoken: {anntoken}')
             sample_ann = nusc.get('sample_annotation', anntoken)
             # nusc.render_annotation(anntoken)
             # plt.show()
@@ -120,35 +250,63 @@ if __name__ == '__main__':
             sample_record = nusc.get('sample', sample_ann['sample_token'])
             assert 'LIDAR_TOP' in sample_record['data'].keys(), 'Error: No LIDAR_TOP in data, unable to render.'
 
-
             # Figure out which camera the object is fully visible in (this may return nothing).
             boxes, cam = [], []
             cams = [key for key in sample_record['data'].keys() if 'CAM' in key]
             tgt_cams = []
             for cam in cams:
-                _, boxes, _ = nusc.get_sample_data(sample_record['data'][cam], box_vis_level=BoxVisibility.ALL,
-                                                   selected_anntokens=[anntoken])
+                data_path, boxes, camera_intrinsic = nusc.get_sample_data(sample_record['data'][cam],
+                                                                          box_vis_level=BoxVisibility.ALL,
+                                                                          selected_anntokens=[anntoken])
                 if len(boxes) > 0:
-
                     assert len(boxes) > 0, 'Error: Could not find image where annotation is visible. ' \
                                                'Try using e.g. BoxVisibility.ANY.'
                     assert len(boxes) < 2, 'Error: Found multiple annotations. Something is wrong!'
 
-                    fig, axes = plt.subplots(figsize=(9, 9))
+                    fig, axes = plt.subplots(1, 2, figsize=(18, 9))
                     tgt_cams.append(cam)
                     camtoken = sample_record['data'][cam]
 
                     # Plot CAMERA view.
-                    data_path, boxes, camera_intrinsic = nusc.get_sample_data(camtoken, selected_anntokens=[anntoken])
+                    # data_path, boxes, camera_intrinsic = nusc.get_sample_data(camtoken, selected_anntokens=[anntoken])
                     im = Image.open(data_path)
-                    axes.imshow(im)
-                    axes.set_title(nusc.get('sample_data', camtoken)['channel'])
-                    axes.axis('off')
-                    axes.set_aspect('equal')
+                    axes[0].imshow(im)
+                    axes[0].set_title(nusc.get('sample_data', camtoken)['channel'])
+                    axes[0].axis('off')
+                    axes[0].set_aspect('equal')
                     for box in boxes:
                         c = np.array(nusc.colormap[box.name]) / 255.0
-                        box.render(axes, view=camera_intrinsic, normalize=True, colors=(c, c, c))
+                        box.render(axes[0], view=camera_intrinsic, normalize=True, colors=(c, c, c))
 
-                    plt.show()
+                    # visualize pred instance mask
+                    pan_file = os.path.join(panoptic_folder, cam, 'panoptic', os.path.basename(data_path)[:-4]+'.png')
+                    pan_img = np.asarray(Image.open(pan_file))
+                    pan_label = img2pan(pan_img)
+                    ins_vis = pan2ins_vis(pan_label, name2label[cityscape_cat][2], divisor)
+
+                    axes[1].imshow(ins_vis)
+                    axes[1].set_title('pred instance')
+                    axes[1].axis('off')
+                    axes[1].set_aspect('equal')
+                    for box in boxes:
+                        c = np.array(nusc.colormap[box.name]) / 255.0
+                        corners = view_points(box.corners(), view=camera_intrinsic, normalize=True)[:2, :]
+                        min_x = np.min(corners[0, :])
+                        max_x = np.max(corners[0, :])
+                        min_y = np.min(corners[1, :])
+                        max_y = np.max(corners[1, :])
+                        rect = patches.Rectangle((min_x, min_y), max_x-min_x, max_y-min_y,
+                                                 linewidth=2, edgecolor='y', facecolor='none')
+                        axes[1].add_patch(rect)
+                        tgt_ins_id, tgt_ins_cnt = tgt_instance(pan_label, name2label[cityscape_cat][2],
+                                                               [min_x, min_y, max_x, max_y])
+                        if tgt_ins_id is not None:
+                            area_ratio = float(tgt_ins_cnt) / (max_x - min_x) / (max_y - min_y)
+                            print(f'        tgt instand id: {tgt_ins_id}, num of pixels: {tgt_ins_cnt}, area ratio: {area_ratio}')
+                        else:
+                            print(f'        no tgt instance found')
+
+                        # box.render(axes[1], view=camera_intrinsic, normalize=True, colors=(c, c, c))
+                    plt.show()  # Press Q to quite the figure
                     # plt.waitforbuttonpress()
                     # plt.close('all')
