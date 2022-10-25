change data pose err generation, fixed changes but with random signs

src/data_nuscenes.py
@@ -271,7 +271,7 @@ class NuScenesData:
                  img_w=1600,
                  add_pose_err=False,
                  max_rot_pert=0.2,
-                 max_t_pert=0.1,
+                 max_t_pert=0.001,
                  debug=False,
                  ):
         """
@@ -408,11 +408,13 @@ class NuScenesData:
                     # ATTENTION: add Rot error in the object's coordinate, and T error
                     if self.add_pose_err:
                         # only consider yaw error and distance error
-                        yaw_err = random.uniform(-self.max_rot_pert, self.max_rot_pert)
+                        # yaw_err = random.uniform(-self.max_rot_pert, self.max_rot_pert)
+                        yaw_err = random.choice([1., -1.]) * self.max_rot_pert
                         rot_err = np.array([[np.cos(yaw_err), -np.sin(yaw_err), 0.],
                                             [np.sin(yaw_err), np.cos(yaw_err), 0.],
                                             [0., 0., 1.]]).astype(np.float32)
-                        trans_err_ratio = random.uniform(1.0-self.max_t_pert, 1.0+self.max_t_pert)
+                        # trans_err_ratio = random.uniform(1.0-self.max_t_pert, 1.0+self.max_t_pert)
+                        trans_err_ratio = 1. + random.choice([1., -1.]) * self.max_t_pert
                         obj_center_w_err = obj_center * trans_err_ratio
                         obj_orientation_w_err = obj_orientation @ rot_err
                         R_c2o_w_err = obj_orientation_w_err.transpose()
@@ -589,11 +591,13 @@ class NuScenesData:
                         # ATTENTION: add Rot error in the object's coordinate, and T error
                         if self.add_pose_err:
                             # only consider yaw error and distance error
-                            yaw_err = random.uniform(-self.max_rot_pert, self.max_rot_pert)
+                            # yaw_err = random.uniform(-self.max_rot_pert, self.max_rot_pert)
+                            yaw_err = random.choice([1., -1.]) * self.max_rot_pert
                             rot_err = np.array([[np.cos(yaw_err), -np.sin(yaw_err), 0.],
                                                 [np.sin(yaw_err), np.cos(yaw_err), 0.],
                                                 [0., 0., 1.]]).astype(np.float32)
-                            trans_err_ratio = random.uniform(1.0 - self.max_t_pert, 1.0 + self.max_t_pert)
+                            # trans_err_ratio = random.uniform(1.0-self.max_t_pert, 1.0+self.max_t_pert)
+                            trans_err_ratio = 1. + random.choice([1., -1.]) * self.max_t_pert
                             obj_center_w_err = obj_center * trans_err_ratio
                             obj_orientation_w_err = obj_orientation @ rot_err
                             R_c2o_w_err = obj_orientation_w_err.transpose()

src/optimizer_nuscenes.py
@@ -232,7 +232,7 @@ class OptimizerNuScenes:
             self.optimized_ann_flag[anntoken] = 1
             self.save_opts(batch_idx)
 
-    def optimize_objs_w_pose(self, lr=1e-2, lr_half_interval=10, save_img=True, roi_margin=5, shapenet_obj_cood=True, sym_aug=False, obj_sz_reg=False, euler_rot=False):
+    def optimize_objs_w_pose(self, lr=1e-2, lr_half_interval=10, save_img=True, roi_margin=5, shapenet_obj_cood=True, sym_aug=True, obj_sz_reg=True, euler_rot=False):
         """
             Optimize on each annotation frame independently
         """
@@ -291,7 +291,7 @@ class OptimizerNuScenes:
 
             # First Optimize
             self.set_optimizers_w_poses(shapecode, texturecode, rot_vec, trans_vec, code_stop_nopts=code_stop_nopts)
-            # self.set_optimizers_w_euler_poses_model(shapecode, texturecode, euler_angles_vec, trans_vec)
+            # self.set_optimizers_w_poses_model(shapecode, texturecode, rot_vec, trans_vec)
             est_poses = torch.zeros((1, 3, 4), dtype=torch.float32)
             while self.nopts < self.num_opts:
                 self.opts.zero_grad()
@@ -424,7 +424,7 @@ class OptimizerNuScenes:
                 self.nopts += 1
                 if self.nopts % lr_half_interval == 0:
                     self.set_optimizers_w_poses(shapecode, texturecode, rot_vec, trans_vec, code_stop_nopts=code_stop_nopts)
-                    # self.set_optimizers_w_poses_model(shapecode, texturecode, euler_angles_vec, trans_vec)
+                    # self.set_optimizers_w_poses_model(shapecode, texturecode, rot_vec, trans_vec)
 
             # Save the optimized codes
             self.optimized_shapecodes[instoken] = shapecode.detach().cpu()
@@ -992,7 +992,7 @@ class OptimizerNuScenes:
         lr = self.get_learning_rate()
         # if code_stop_nopts is not None and self.nopts >= code_stop_nopts:
         #     self.opts = torch.optim.AdamW([
-        #         {'params': euler_angles, 'lr': lr},
+        #         {'params': rots, 'lr': lr},
         #         {'params': trans, 'lr': lr}
         #     ])
         # else: